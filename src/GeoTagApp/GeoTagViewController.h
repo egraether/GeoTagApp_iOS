5c9b6599b481d56490d84f0bf1efd3b23f583043
Eberhard Gräther <ebsigraether@hotmail.com>
2011-03-28 00:15:35 +0200

    old version

---
+++ src/GeoTagApp/GeoTagViewController.h
@@ -0,0 +1,14 @@
+
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+
+@interface GeoTagView : UIView {
+    
+    IBOutlet UILabel* name;
+    UIImageView* image;
+    
+}
+
+@end
